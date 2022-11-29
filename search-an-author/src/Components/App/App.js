be7966cfe3ee0c470c9701105a6ee6f4fe7b23f2
Vinícius Queiróz de Oliveira <vqovinicius@gmail.com>
2022-11-29 16:52:46 +0100

    code cleanup from template to sketch of our app

---
+++ search-an-author/src/Components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './app.css';
+
+class App extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+        <h1>AUTHOR SEARCH</h1>
+        <div className='App'></div>
+      </div>
+    )
+  }
+}
+
+export default App;
